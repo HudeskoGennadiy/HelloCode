574bf08f70889a2a1e51836d54bdff3efeefae2f
Gennadiy Khudesko <gennadiy.18224@gmail.com>
2022-09-03 13:51:35 +0300

    add new folder Example003_Sum

---
+++ Example001_HelloConsole/Example003_Sum/Program.cs
@@ -0,0 +1,8 @@
+﻿// Алгоритм - Начало - Определить namberA - Определить namberB - Вывод namberA + namberB - Конец
+
+int namberA = 31;
+int namberB = 5;
+int result = namberA + namberB;
+Console.WriteLine(result);
+
+
