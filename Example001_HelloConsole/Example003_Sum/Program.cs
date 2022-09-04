c9a0bcea7cf873a7e1be5bce9377c4963780eeb9
Gennadiy Khudesko <gennadiy.18224@gmail.com>
2022-09-04 13:44:20 +0300

    add random

--- Example001_HelloConsole/Example003_Sum/Program.cs
+++ Example001_HelloConsole/Example003_Sum/Program.cs
@@ -1,7 +1,9 @@
 ﻿// Алгоритм - Начало - Определить namberA - Определить namberB - Вывод namberA + namberB - Конец
 
-int namberA = 31;
-int namberB = 5;
+int namberA = new Random().Next(1, 10); // 1 2 3 4 .... 9
+Console.WriteLine(namberA);
+int namberB = new Random().Next(1, 10);
+Console.WriteLine(namberB);
 int result = namberA + namberB;
 Console.WriteLine(result);
 
